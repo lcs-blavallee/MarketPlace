862f49dc4f6aca8184f420c71c2810549ac2661b
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-01-22 21:04:32 -0500

    Initial Commit

---
+++ MarketPlace/MarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketPlaceApp.swift
+//  MarketPlace
+//
+//  Created by Benjamin Lavallee on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct MarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
