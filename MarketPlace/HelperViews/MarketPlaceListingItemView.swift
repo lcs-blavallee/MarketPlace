e52c89f20621f5c5bdb1fa66b69b65557c4f3d65
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-06-06 13:07:59 -0400

    Used my old static interface but now its connected to the database

---
+++ MarketPlace/HelperViews/MarketPlaceListingItemView.swift
@@ -0,0 +1,48 @@
+//
+//  MarketPlaceListingItemView.swift
+//  MarketPlace
+//
+//  Created by Benjamin Lavallee on 2024-06-06.
+//
+
+import SwiftUI
+
+struct MarketPlaceListingItemView: View {
+    let listing: MarketPlaceListing
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            RoundedRectangle(cornerRadius: 10.0)
+                .frame(width: 150, height: 200)
+//            Image(listing.thumbnail)
+//                .resizable()
+//                .scaledToFill()
+//                .frame(width: 150, height: 200)
+//                .clipped()
+//                .cornerRadius(10)
+            
+            Text(priceString(from: listing.price)) // Correctly calling the function
+                .foregroundColor(.black)
+                .bold()
+                .lineLimit(1)
+
+            Text(listing.title)
+                .foregroundColor(.black)
+                .lineLimit(1)
+
+        }
+        .padding([.leading, .trailing, .top])
+    }
+
+    private func priceString(from price: Double) -> String {
+        return price.formatted(.currency(code: "CAD").presentation(.narrow))
+    }
+
+}
+
+//#Preview {
+//    
+//    let viewModel = MarketPlaceViewModel()
+//    
+//    return MarketPlaceListingItemView(listing: viewModel.listings.first!)
+//}
--- MarketPlace/MainViews/SimpleListingView.swift
+++ MarketPlace/MainViews/SimpleListingView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct SimpleListingView: View {
     
     @State private var viewModel: MarketPlaceViewModel = MarketPlaceViewModel()
+    private var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
     
     var body: some View {
         NavigationStack {
-            List(viewModel.listings) { listing in
-                Text(listing.title)
+            
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 16) {
+                    ForEach(viewModel.listings) { listing in
+                        MarketPlaceListingItemView(listing: listing)
+                    }
+                }
+                .padding(.horizontal)
             }
+            
+            
         }
     }
     
