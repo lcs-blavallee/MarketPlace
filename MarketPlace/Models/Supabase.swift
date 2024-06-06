ab99e429ef0a1b790cf713ae75ef0d322145cd34
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-06-06 11:48:31 -0400

    Pointing to my database

--- MarketPlace/Models/Supabase.swift
+++ MarketPlace/Models/Supabase.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://fosalaujbksvhelafhrr.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvc2FsYXVqYmtzdmhlbGFmaHJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxODA3NjUsImV4cCI6MjAzMjc1Njc2NX0.NM5EVak4bsO1QaqavUbxyf4xvVyPLXpfUOtsim1HDv8"
+)
