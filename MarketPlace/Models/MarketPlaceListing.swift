7752b87659cd85a782b85389c6d2fdde06e016ac
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-06-11 13:44:51 -0400

    Adjusted model for TodoItem to match new column in todos table for tracking image URL.

--- MarketPlace/Models/MarketPlaceListing.swift
+++ MarketPlace/Models/MarketPlaceListing.swift
@@ -62,11 +62,16 @@ struct MarketPlaceListing: Identifiable, Codable {
         var id: Int?
         var filename: String
         var listingId: Int?
+        var imageURL: String?
+
+        // When decoding and encoding from JSON, translate snake_case
+        // column names into camelCase
         
         enum CodingKeys: String, CodingKey {
             case id
             case filename
             case listingId = "listing_id"
+            case imageURL = "image_url"
         }
     }
     
